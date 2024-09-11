271f0cc1584cca32161dd25b2caff885b80442ff
Jonathan <jojojow@DESKTOP-LI4U1UP>
2024-09-11 13:41:59 +0700

    boilerplate 2024

---
+++ src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AppExceptionFilter, AppResponseInterceptor } from '@app/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        PORT: Joi.string().required(),
+        WS_PORT: Joi.string().required(),
+        DATABASE_URL: Joi.string().required(),
+        APP_NAME: Joi.string().required(),
+        PREFIX_NAME: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+      }),
+    }),
+    AuthModule,
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AppExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AppResponseInterceptor,
+    },
+  ]
+})
+export class AppModule {}
