75cd7270ab46e38b3acee395023f862109febe2e
Prince JO <jojojow@DESKTOP-LI4U1UP>
2024-09-27 14:15:03 +0700

    feat: update boilerplate

--- prisma/migrations/20240814134953_alter/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "ChessCom" ADD COLUMN     "player1GameHistoryUri" TEXT,
-ADD COLUMN     "player1RelatedGameIndex" INTEGER,
-ADD COLUMN     "player2GameHistoryUri" TEXT,
-ADD COLUMN     "player2RelatedGameIndex" INTEGER;
